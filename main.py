d02577016c4382772a21cd3cf0807f7072a98bc3
Jaidev Ishwarbhai Sondagar <116446884+jAiDeV-21@users.noreply.github.com>
2024-10-28 16:38:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,35 @@
 import streamlit as st
 import requests
-import os
 from datetime import datetime
 
-def process_path(download_path):
-    # change '\' to '/'
-    download_path = download_path.replace("\\", '/')
+def generate_links(course, sessions, start_year, current_year, subject_code):
+    if subject_code:
+        st.text("Click to open.")
+        for session in sessions:
+            for year in range(start_year, current_year):
+                url = format_url(course=course, session=session, year=year, subject_code=subject_code)
+                req = requests.get(url)
+                if req.status_code == 200:
+                    st.link_button(label=f"{subject_code}_{session}{year}", url=url)
 
-    if download_path[-1] == '/':
-        download_path = download_path[:-1]
+        st.text("Links generated successfully.")
+    else:
+        st.error("Something went wrong.")
 
-    return download_path
 
-
-def format_url(session, year, subject_code):
-    return f"https://gtu.ac.in/uploads/{session}{year}/BE/{subject_code}.pdf"
+def format_url(course, session, year, subject_code):
+    return f"https://gtu.ac.in/uploads/{session}{year}/{course}/{subject_code}.pdf"
 
 
 st.title("JustClic!")
 
-# Ask the user to enter a download directory path
-download_path = st.text_input("Enter the path where you want to save the downloaded files:")
-subject_code = st.text_input("Enter subject code")
-
+courses = ['AF', 'BA', 'BB', 'BC', 'BD', 'BE', 'BESP', 'BH', 'BI', 'BL', 'BM', 'BN', 'BP', 'BPSP', 'BS', 'BT', 'BV', 'CI', 'CS', 'DA', 'DB', 'DH', 'DI', 'DISP', 'DM', 'DP', 'DS', 'DV', 'EP', 'FD', 'HM', 'IB', 'IC', 'IM', 'MA', 'MB', 'MC', 'MCSP', 'MD', 'ME', 'MH', 'ML', 'MN', 'MP', 'MR', 'MS', 'MT', 'MV', 'PB', 'PD', 'PH', 'PM', 'PP', 'PR', 'TE', 'VP']
 sessions = ('W', 'S')
 start_year = 2018
 current_year = datetime.now().year
 
-# Check if the directory exists then download, otherwise display a message
-if st.button("Download"):
-    if download_path and subject_code:
-        download_path = process_path(download_path=download_path)
+course_option = st.selectbox("Enter your course", options=courses)
+subject_code = st.text_input("Enter subject code")
 
-        if os.path.isdir(download_path):
-            for session in sessions:
-                for year in range(start_year, current_year):
-                    url = format_url(session=session, year=year, subject_code=subject_code)
-                    
-                    # download paper
-                    req = requests.get(url)
-                    if req.status_code == 200:
-                        with open(f"{download_path}/{subject_code}_{session}{year}.pdf", 'wb') as file:
-                            file.write(req.content)
-
-            st.text("Papers downloaded successfully.")
-        else:
-            st.error("Something went wrong.")
+if st.button("Generate"):
+    generate_links(course=course_option, sessions=sessions, start_year=start_year, current_year=current_year, subject_code=subject_code)
